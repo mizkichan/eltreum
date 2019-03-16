1f995fb945b890d577d71a27058a985de4e9502b
Mizuki HAGIMOTO <hagimotomizuki@gmail.com>
2019-03-16 22:56:54 +0900

    pytumblr is buggy.

--- post.py
+++ post.py
@@ -1,7 +1,20 @@
 import sys
+from requests_oauthlib import OAuth1Session
 import config
-from pytumblr import TumblrRestClient
 
-client = TumblrRestClient(config.consumer_key, config.consumer_secret,
-                          config.oauth_token, config.oauth_secret)
-client.create_text(config.blogname, body=sys.stdin.read())
+API_ROOT = 'https://api.tumblr.com/v2/'
+
+client = OAuth1Session(
+    client_key=config.consumer_key,
+    client_secret=config.consumer_secret,
+    resource_owner_key=config.oauth_token,
+    resource_owner_secret=config.oauth_secret)
+client.post(
+    f'{API_ROOT}/blog/{config.blogname}/posts',
+    json={
+        'content': [{
+            'type': 'text',
+            'text': sys.stdin.read()
+        }],
+        'send_to_twitter': True
+    })
